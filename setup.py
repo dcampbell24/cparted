5f0a9fbde2444f8d6f9201eded61d42fbb165d70
David Campbell <davekong@archlinux.us>
2011-03-15 03:25:45 -0400

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,29 @@
+import shutil
+import os
+
+from distutils.core import setup
+
+try:
+    shutil.copy('cparted.py', 'cparted')
+
+    setup(name='cparted',
+          version='0.1dev',
+          description='Curses based disk partition manipulation program',
+          author='David Campbell',
+          author_email='davekong@archlinux.us',
+          url='https://github.com/davekong/cparted',
+          scripts=['cparted'],
+          requires=['pyparted'],
+          data_files=[('/usr/share/man/man8', ['cparted.8']),
+                      ('/usr/share/doc/cparted', ['README.md'])
+                     ],
+          classifiers=[
+              'Environment :: Console :: Curses',
+              'Intended Audience :: System Administrators',
+              'License :: OSI Approved :: GNU General Public License (GPL)',
+              'Natural Language :: English',
+              'Programming Language :: Python :: 2.7',
+          ],
+         )
+finally:
+    os.remove('cparted')
