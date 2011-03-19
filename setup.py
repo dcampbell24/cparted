832d9e023ff51d698d6c34d2bc3d2f217431f79a
David Campbell <davekong@archlinux.us>
2011-03-19 00:44:22 -0400

    Update the setup.py file
    Added more metadata, and made the file work when run in source distributions.

--- setup.py
+++ setup.py
@@ -5,25 +5,27 @@ from distutils.core import setup
 
 try:
     shutil.copy('cparted.py', 'cparted')
+except Exception:
+    pass
 
-    setup(name='cparted',
-          version='0.1',
-          author='David Campbell',
-          author_email='davekong@archlinux.us',
-          url='https://github.com/davekong/cparted',
-          description='Curses based disk partition manipulation program',
-          classifiers=[
-              'Environment :: Console :: Curses',
-              'Intended Audience :: System Administrators',
-              'License :: OSI Approved :: GNU General Public License (GPL)',
-              'Natural Language :: English',
-              'Programming Language :: Python :: 2.7',
-          ],
-          data_files=[('/usr/share/man/man8', ['cparted.8']),
-                      ('/usr/share/doc/cparted', ['README.md'])
-                     ],
-          requires=['pyparted'],
-          scripts=['cparted'],
-         )
-finally:
-    os.remove('cparted')
+setup(name='cparted',
+      version='0.1',
+      author='David Campbell',
+      author_email='davekong@archlinux.us',
+      url='https://github.com/davekong/cparted',
+      description='Curses based disk partition manipulation program',
+      long_description="""\
+      cparted is a curses based disk partition manipulation program that aims
+      to replace cfdisk by providing a friendly curses interface with the
+      partition manipulation power of libparted. cparted is written in Python,
+      and thus makes use of libparted through pyparted.""",
+      classifiers=['Environment :: Console :: Curses',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Natural Language :: English',
+                   'Programming Language :: Python :: 2.7'],
+      license='GPL',
+      data_files=[('/usr/share/man/man8', ['cparted.8']),
+                  ('/usr/share/doc/cparted', ['README.md'])],
+      requires=['pyparted'],
+      scripts=['cparted'])
